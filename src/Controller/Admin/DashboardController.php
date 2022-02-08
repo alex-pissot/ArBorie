e30216a4c73f400f45a936d705a88d0bf5475986
Alex Pissot <ylex@hotmail.fr>
2022-02-08 16:44:27 +0100

    ad admin + sécurisation admin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Trails;
+use App\Entity\Trees;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        return parent::index();
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Arborie');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Arbres', 'fas fa-list', Trees::class);
+        yield MenuItem::linkToCrud('Parcours', 'fas fa-list', Trails::class);
+        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-list', User::class);
+
+    }
+}
