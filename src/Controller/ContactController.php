fa183e15c9f79d64978d428f04435e6f9a50c1bc
Damien Bussy <damien.bussy@etu.unilim.fr>
2022-03-17 13:05:28 +0100

    formulaire de conctact, symfony mailer

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,51 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
 
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'contact')]
-    public function index(): Response
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function index(Request $request, MailerInterface $mailer)
     {
+        $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $contactFormData = $form->getData();
+
+            $message = (new Email())
+                ->from($contactFormData['email'])
+                ->to('bussydmn@mail.com')
+                ->subject('test')
+                ->text('Sender : '.$contactFormData['email'].\PHP_EOL.
+                    $contactFormData['message'],
+                    'text/plain');
+            $mailer->send($message);
+
+
+
+
+            $this->addFlash('success', 'Your message has been sent');
+
+            return $this->redirectToRoute('contact');
+        }
+
+
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'our_form' => $form->createView()
         ]);
     }
+
 }
